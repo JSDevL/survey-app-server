2b23d390204b7e988be0c266bc2ebd1fc66e385a
arfath <arfath@niiu.de>
2018-09-08 22:35:00 +0530

    SWITCHED: using gradle with groovy DSL for builds

---
+++ survey-app-web/src/main/java/com/arfath/surveyapp/web/Boot.java
@@ -0,0 +1,11 @@
+package com.arfath.surveyapp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot {
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class);
+    }
+}
